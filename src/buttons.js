3ed12da4fadfcde8c43d2872a1d43e2d3e6ddd26
Roman Juchimcuk <romasprojects@gmail.com>
2019-07-03 21:30:50 +0300

    Add buttons, remove unnecessary files

---
+++ src/buttons.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Buttons extends React.Component {
+    render(){
+        return(
+            <div>
+                <button>C</button>
+                <button>&plusmn;</button>
+                <button>%</button>
+                <button>&divide;</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>&times;</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>-</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>+</button>
+                <button>0</button>
+                <button>,</button>
+                <button>AC</button>
+                <button>=</button>
+            </div>
+        )
+    }
+}
+
+export default Buttons;
---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Buttons from './buttons';
+
+
+class Calculator extends React.Component {
+    render(){
+        return (
+            <div>
+                <Buttons />
+            </div>
+        )
+        }
+}
+
+export default Calculator;
