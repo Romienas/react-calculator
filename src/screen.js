b2328834d10425927cfe9d82c7ff152a7004fe60
Roman Juchimcuk <romasprojects@gmail.com>
2019-07-08 21:49:23 +0300

    add close, minimize and expad buttons

--- src/screen.js
+++ src/screen.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faExpandArrowsAlt, 
+    faWindowMinimize, 
+    faTimes 
+} from '@fortawesome/free-solid-svg-icons';
 
 class Screen extends React.Component {
     render(){
         return(
             <div className='screen'>
                 <div className='window-buttons'>
-                    <div className='window-button'>x</div>
-                    <div className='window-button'>-</div>
-                    <div className='window-button'>x</div>
+                    <div className='window-button'>
+                        <FontAwesomeIcon className='fa-font' icon={faTimes} />
+                    </div>
+                    <div className='window-button'>
+                        <FontAwesomeIcon className='fa-font' icon={faWindowMinimize} />
+                    </div>
+                    <div className='window-button'>
+                        <FontAwesomeIcon className='fa-font' icon={faExpandArrowsAlt} />
+                    </div>
                 </div>
                 <div className='calculation'>1.764 / 14</div>
                 <div className='equal-sign'>=</div>
